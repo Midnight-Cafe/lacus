42606e5930d1f58ec569fcbb12ac82e69f6361e7
Joel Aro <joel@greenspacehealth.com>
2021-08-17 22:48:33 -0700

    Add event handler for new joiners

--- bot.py
+++ bot.py
@@ -1,18 +1,43 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+class LacusBaseException(Exception):
+    pass
 
 
-client = MyClient()
-client.run(os.environ.get("TOKEN"))
+class IncorrectConfigurationError(LacusBaseException):
+    pass
+
+
+required_env = [
+    "DISCORD_TOKEN",
+    "GUILD_ID",
+    "ROLE_ON_JOIN_ID",
+    "ROLE_ON_JOIN_NAME"
+]
+env_not_found = set(required_env) - set(os.environ)
+if len(env_not_found) > 0:
+    raise IncorrectConfigurationError(', '.join(env_not_found))
+
+guild_id = os.environ.get("GUILD_ID")
+role_on_join_id = os.environ.get("ROLE_ON_JOIN_ID")
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(intents=intents, command_prefix="!")
+
+
+@bot.event
+async def on_member_join(member: discord.Member):
+    guild = await bot.fetch_guild(guild_id)
+    role: discord.Role = guild.get_role(role_on_join_id)
+    await member.add_roles(role)
+
+token = os.environ.get("DISCORD_TOKEN")
+bot.run(token)
