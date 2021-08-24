9b1fbd2d7b70c386ed13e3866af08493ff0f9f19
Darian Sharma <80566729+dariansharma@users.noreply.github.com>
2021-08-24 01:43:22 -0500

    Implementing Issue #6 (#12)
    * timed channel messages
    
    * custom interval seconds
    
    * scheduled messages moved to json file
    
    * using bot event api and on_ready
    
    * Update app.py
    
    * printer made public, loop, naming & casing fixed
    
    * added .env.empty template back
    
    * decorator initializing moved to own function

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import discord
-from discord.ext import commands
+import asyncio
+import json
+import os
+from discord.ext import commands, tasks
+from cogs.scheduledmessage import ScheduledMessage
 
 from lacus.setup import LacusConfig
 
@@ -19,6 +23,18 @@ async def on_member_join(member: discord.Member):
 async def add_role(member: discord.Member, role_id: int):
     role: discord.Role = member.guild.get_role(config.ROLE_ON_JOIN_ID)
     await member.add_roles(role)
+
+@bot.event
+async def on_ready():
+    def message_cog_maker(prompt):
+        if(prompt['type']=='question'):
+            bot.add_cog(ScheduledMessage(bot, prompt['message'], prompt['channel_id'], prompt['interval_seconds']))
+    if (os.path.isfile('./messages.json')):
+        messages_file = open('./messages.json', 'r')
+        messages_json = json.loads(messages_file.read())
+        for prompt in messages_json['prompts']:
+            message_cog_maker(prompt)
+        messages_file.close()
 
 
 bot.run(config.DISCORD_TOKEN)
