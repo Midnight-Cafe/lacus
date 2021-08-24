84252a160f559a59e4f60daaa0b11b5696c48ee4
Joel Aro <joel@greenspacehealth.com>
2021-08-24 00:47:11 -0700

    Define scheduled messages channel in the environment variables file

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import discord
-import asyncio
 import json
 import os
-from discord.ext import commands, tasks
+from discord.ext import commands
 from cogs.scheduledmessage import ScheduledMessage
 
 from lacus.setup import LacusConfig
 
 config = LacusConfig()
 
-bot = commands.Bot(
-    intents=config.INTENTS,
-    command_prefix=config.BOT_PREFIX
-)
+bot = commands.Bot(intents=config.INTENTS, command_prefix=config.BOT_PREFIX)
 
 
 @bot.event
@@ -24,15 +20,24 @@ async def add_role(member: discord.Member, role_id: int):
     role: discord.Role = member.guild.get_role(config.ROLE_ON_JOIN_ID)
     await member.add_roles(role)
 
+
 @bot.event
 async def on_ready():
     def message_cog_maker(prompt):
-        if(prompt['type']=='question'):
-            bot.add_cog(ScheduledMessage(bot, prompt['message'], prompt['channel_id'], prompt['interval_seconds']))
-    if (os.path.isfile('./messages.json')):
-        messages_file = open('./messages.json', 'r')
+        if prompt["type"] == "question":
+            bot.add_cog(
+                ScheduledMessage(
+                    bot,
+                    prompt["message"],
+                    config.SCHEDULED_MESSAGES_CHANNEL_ID,
+                    prompt["interval_seconds"],
+                )
+            )
+
+    if os.path.isfile("./messages.json"):
+        messages_file = open("./messages.json", "r")
         messages_json = json.loads(messages_file.read())
-        for prompt in messages_json['prompts']:
+        for prompt in messages_json["prompts"]:
             message_cog_maker(prompt)
         messages_file.close()
 
--- cogs/scheduledmessage.py
+++ cogs/scheduledmessage.py
@@ -1,7 +1,14 @@
 from discord.ext import tasks, commands
 
+
 class ScheduledMessage(commands.Cog):
-    def __init__(self, bot: commands.Bot, message: str, channel_id: int, interval_seconds: int):
+    def __init__(
+        self,
+        bot: commands.Bot,
+        message: str,
+        channel_id: int,
+        interval_seconds: int,
+    ):
         self.bot = bot
         self.message = message
         self.channel_id = channel_id
@@ -12,7 +19,9 @@ class ScheduledMessage(commands.Cog):
         self.task_send_message.cancel()
 
     def initialize_send_message(self):
-        self.task_send_message = tasks.loop(seconds=self.interval_seconds)(self.send_message)
+        self.task_send_message = tasks.loop(seconds=self.interval_seconds)(
+            self.send_message
+        )
         self.task_send_message.before_loop(self.before_send_message)
         self.task_send_message.start()
 
