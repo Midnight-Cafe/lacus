bee6cc9960c1c00bf5653f0dde415fa9a476419c
Joey Lim <58991251+jyorien@users.noreply.github.com>
2021-09-06 15:30:51 +0800

    Implemented timezone conversion (#24)
    * Added pytz lib, Implemented base function for timezone conversion
    
    - Added pytz in requirements.txt
    - Bot takes input !event <name> <dd/mm/yyyy hh:mm> <timezone>
    - Responds with
    Event name:
    Scheduled for: <List of (hardcoded) 'Timezone: Date Time'>
    
    * [Feature] Add Scheduled Messages (#25)
    
    * Implementing Issue #6 (#12)
    
    * timed channel messages
    
    * custom interval seconds
    
    * scheduled messages moved to json file
    
    * using bot event api and on_ready
    
    * Update app.py
    
    * printer made public, loop, naming & casing fixed
    
    * added .env.empty template back
    
    * decorator initializing moved to own function
    
    * Cleanup and refactor (#16)
    
    * Update requirements.txt
    
    * Cleanup and refactor
    
    * Add suffix -cog to Cog implementations
    
    * Populate messages.json (#13)
    
    * Populate messages.json
    
    * Define scheduled messages channel in the environment variables file
    
    * Add ability to add and list scheduled messages (#21)
    
    * Populate messages.json
    
    * Define scheduled messages channel in the environment variables file
    
    * Huge refactor work to ScheduledMessagesCog
    
    * Add tests and update black config
    
    * Cleanup
    
    Co-authored-by: Darian Sharma <80566729+dariansharma@users.noreply.github.com>
    
    * Added pytz lib, Implemented base function for timezone conversion
    
    - Added pytz in requirements.txt
    - Bot takes input !event <name> <dd/mm/yyyy hh:mm> <timezone>
    - Responds with
    Event name:
    Scheduled for: <List of (hardcoded) 'Timezone: Date Time'>
    
    * refactored timezone converter into a cog
    
    * Implemented command to add a timezone
    
    - !time add_timezone <timezone> adds a timezone to the list of timezones
    
    * Implemented command to delete a timezone
    
    !time delete_timezone <timezone> deletes a timezone to the list of timezones
    
    * Implemented command to list timezones
    
    !time list_timezones lists the list of timezones the bot is currently storing
    
    * fixed typo in message
    
    Co-authored-by: Joel Aro <joelaro.to@gmail.com>
    
    * removed channel in TimeCog
    
    Co-authored-by: Joel Aro <joelaro.to@gmail.com>
    Co-authored-by: Darian Sharma <80566729+dariansharma@users.noreply.github.com>

---
+++ lacus/cogs/time.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+import datetime
+import pytz
+
+class TimeCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.timezones = ['America/Los_Angeles', 'Canada/Central', 'Asia/Singapore']
+
+    @commands.group()
+    async def time(self, ctx: commands.Context):
+        pass
+
+    @time.group()
+    async def convert(self, ctx: commands.Context, date, time ,tz):
+        day, month, year = date.split("/")
+        hour, minute = time.split(":")
+        timezone = pytz.timezone(tz.strip())
+
+        dt = timezone.localize(datetime.datetime(int(year), int(month), int(day), int(hour), int(minute)))
+        date_format = "%d-%b-%Y %H:%M"
+
+        reply_message = f'```{date} {time} in {tz} converted to other timezones:\n'
+        for zone in self.timezones:
+            localDateTime = dt.astimezone(pytz.timezone(zone))
+            reply_message += f'{zone:20s}: {localDateTime.strftime(date_format)}\n'
+        reply_message +='```'
+        await ctx.send(reply_message)
+        
+    @time.group()
+    async def add_timezone(self, ctx:commands.Context, timezone):
+        if (timezone in self.timezones):
+            await ctx.send("Timezone has already been added!")
+            return
+        
+        if (timezone not in pytz.all_timezones):
+            await ctx.send("Please enter a valid timezone name.\n A list of timezones names can be found here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones ")
+            return
+        
+        self.timezones.append(timezone)
+        await ctx.send(f'{timezone} has been added!')
+
+    @time.group()
+    async def delete_timezone(self, ctx:commands.Context, timezone):
+        if (timezone not in self.timezones):
+            await ctx.send(f'{timezone} is not in the list!')
+            return
+        
+        self.timezones.remove(timezone)
+        await ctx.send(f'{timezone} has been deleted.')
+
+    @time.group()
+    async def list_timezones(self, ctx:commands.Context):
+        message = f'```List of timezones: \n'
+        for timezone in self.timezones:
+            message +=  f'{timezone}\n'
+        message += f'```'
+        await ctx.send(message)
+        
